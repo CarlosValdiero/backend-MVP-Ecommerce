1a1939ec3fbb4420bc30048578bd3cd86988efd3
Carlos Valdiero <carlos_valdiero@hotmail.com>
2020-01-31 17:33:58 -0300

    configured cors

--- src/app/app.js
+++ src/app/app.js
@@ -2,11 +2,14 @@ require('dotenv').config({
     path: process.env.NODE_ENV == 'test'? '.env.test':'.env'
 });
 const express = require("express");
+const cors = require('cors');
 const routes = require('./routes');
+
 
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
